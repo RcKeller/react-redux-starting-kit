1222939575742723b100c84da65df7fe23a6d972
Emmanuel Morales <emorales0024@gmail.com>
2016-05-20 17:01:15 -0500

    Step 4:  Create '~/containers/WelcomeCardContainer.jsx'.

---
+++ src/containers/WelcomeCardContainer.jsx
@@ -0,0 +1,6 @@
+import {connect} from 'react-redux';
+import WelcomeCard from '../components/WelcomeCard.jsx';
+
+const mapStateToProps = (state) => (Object.assign({}, state.welcomeCard));
+
+export default connect(mapStateToProps)(WelcomeCard);
