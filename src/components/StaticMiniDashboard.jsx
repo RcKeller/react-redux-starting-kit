aeff4303ce6f1668011796f087b2429e70d64b54
Emmanuel Morales <emorales0024@gmail.com>
2016-05-20 16:41:10 -0500

    Step 2:  Create '~/components/WelcomeCard.jsx' component and refactor 'StaticMiniDashboard.jsx' to use it.

--- src/components/StaticMiniDashboard.jsx
+++ src/components/StaticMiniDashboard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from '../components/Card.jsx';
+import WelcomeCard from '../components/WelcomeCard.jsx';
 
 export default ({ welcomeMsg, cards }) => (
   <div className="mdl-layout__container">
     <div className="mdl-layout">
       <div className="mdl-layout mdl-color--grey-100">
         <div className="dashboard-wrapper">
-          <Card className="welcome-card" {...welcomeMsg} />
+          <WelcomeCard {...welcomeMsg} />
           <div className="cards-wrapper">
             {cards.map((card, index) => (
               <Card key={index} className="card-item" {...card} />
---
+++ src/components/WelcomeCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from '../components/Card.jsx';
+
+const WelcomeCard = (props) => (
+  <Card className="welcome-card" {...props} />
+);
+
+export default WelcomeCard;
