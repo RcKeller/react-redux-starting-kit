c7fcd02025847b655c6b605a07449bce50384341
Emmanuel Morales <emorales0024@gmail.com>
2016-05-20 17:07:35 -0500

    Step 5:  Create '~/containers/CardListContainers.jsx'.

---
+++ src/containers/CardListContainer.jsx
@@ -0,0 +1,8 @@
+import {connect} from 'react-redux';
+import CardList from '../components/CardList.jsx';
+
+const mapStateToProps = (state) => ({
+  cards: state.cardList
+});
+
+export default connect(mapStateToProps)(CardList);
