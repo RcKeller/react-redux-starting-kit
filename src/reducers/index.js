edc3eb5d770ef4c430e9f5494566236aa8d56731
Emmanuel Morales <emorales0024@gmail.com>
2016-05-22 16:58:49 -0500

    Step 10:  Update '~/reducers/index.js' to combine reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
+import welcomeCard from '../reducers/welcomeCardReducer';
+import cardList from '../reducers/cardListReducer';
 
-// import all custom reducers here
-// example
-// import example from '../reducers/exampleReducer';
-
-// combine reducers here, comma separated
-// example
-// const rootReducer = combineReducers({ example });
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({
+  welcomeCard,
+  cardList
+});
 
 export default rootReducer;
