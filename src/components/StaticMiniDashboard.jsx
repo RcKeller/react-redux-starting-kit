3e014932aa1b7165cfb1935e61f6707aeadf31c9
Ryan Keller <rykeller@uw.edu>
2016-12-12 18:51:07 -0800

    Step1 complete.

--- src/components/StaticMiniDashboard.jsx
+++ src/components/StaticMiniDashboard.jsx
@@ -1,30 +1,19 @@
-import React from 'react';
+import React from 'react'
+import Card from '../components/Card.jsx'
 
 export default ({ welcomeMsg, cards }) => (
-  <div className="mdl-layout__container">
-    <div className="mdl-layout">
-      <div className="mdl-layout mdl-color--grey-100">
-        <div className="dashboard-wrapper">
-          <div className="welcome-card">
-            <div className="mdl-card__title"
-              style={{background: 'url(' + welcomeMsg.imgUrl  + ') center/cover'}}>
-              <h2 className="mdl-card__title-text">{welcomeMsg.title}</h2>
-            </div>
-            <div className="mdl-card__supporting-text">{welcomeMsg.msg}</div>
-          </div>
-          <div className="cards-wrapper">
-            {cards.map((card, index) => (
-              <div key={index} className="card-item">
-                <div className="mdl-card__title"
-                  style={{background: 'url(' + card.imgUrl  + ') center/cover'}}>
-                  <h2 className="mdl-card__title-text">{card.title}</h2>
-                </div>
-                <div className="mdl-card__supporting-text">{card.msg}</div>
-              </div>
-            ))}
-          </div>
+  <div className='mdl-layout__container'>
+    <div className='mdl-layout'>
+      <div className='mdl-layout mdl-color--grey-100'>
+        <div className='dashboard-wrapper'>
+          <Card className='welcome-card' {...welcomeMsg} />
+        </div>
+        <div className='cards-wrapper'>
+          {cards.map((card, index) => (
+            <Card key={index} className='card-item' {...card} />
+          ))}
         </div>
       </div>
     </div>
   </div>
-);
+)
